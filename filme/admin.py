d83ae961b65841ddf187fd97dd549417bfb2893e
Marcelo Moreira <marcelolsm34@gmail.com>
2023-06-19 23:56:32 -0300

    check point aula 12
    Todas as alterações não comitadas até a aula 12.

--- filme/admin.py
+++ filme/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Filme
 
 # Register your models here.
+
+# Importar o app Filme aqui no admin para aparecer no admin
+admin.site.register(Filme)
