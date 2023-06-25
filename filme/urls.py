23d5d3a799b0bd5f3729fa9c64c525b76fef1fa2
Marcelo Moreira <marcelolsm34@gmail.com>
2023-06-25 19:09:47 -0300

    Alteração das views dos filmes

--- filme/urls.py
+++ filme/urls.py
@@ -2,9 +2,10 @@
 # Tem que cosntruir uma URL, uma View e um Template (parte visual - arquivos de html)
 
 from django.urls import path
-from .views import Homepage, Homefilmes
+from .views import Homepage, Homefilmes, Detalhesfilme
 
 urlpatterns = [
     path('', Homepage.as_view()),
     path('filmes', Homefilmes.as_view()),
+    path('filmes/<int:pk>', Detalhesfilme.as_view())
 ]
--- filme/views.py
+++ filme/views.py
@@ -1,27 +1,34 @@
 from django.shortcuts import render
 from .models import Filme
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 
 # Create your views here.
-
-# Function Based View
-# def homepage(request):
-#     return render(request, 'homepage.html')
-
 # Class Based View
 
 
+# url - view - html
 class Homepage(TemplateView):
     template_name = 'homepage.html'
-# url - view - html
-# def homefilmes(request):
-#     context = {}
-#     lista_filmes = Filme.objects.all()
-#     context['lista_filmes'] = lista_filmes
-#     return render(request, 'homefilmes.html', context)
 
 
 class Homefilmes(ListView):
     template_name = 'homefilmes.html'
     model = Filme
-    # object_list
+    # object_list -> lista de itens do modelo
+
+
+class Detalhesfilme(DetailView):
+    template_name = 'detalhesfilme.html'
+    model = Filme
+    # object -> 1 item do nosso modelo
+
+
+# Function Based View
+# def homepage(request):
+#     return render(request, 'homepage.html')
+
+# def homefilmes(request):
+#     context = {}
+#     lista_filmes = Filme.objects.all()
+#     context['lista_filmes'] = lista_filmes
+#     return render(request, 'homefilmes.html', context)
